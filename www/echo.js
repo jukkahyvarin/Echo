2455db03c76a61bc14801aeb125a83d07d46776d
jukkahyvarin <jukka.hyvarin@gmail.com>
2014-04-10 12:07:22 +0300

    initial commit

---
+++ www/echo.js
@@ -0,0 +1,14 @@
+var echo = {
+    speak: function(message, successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'Echo', // mapped to our native Java class called "Calendar"
+            'echo', // with this action name
+            [{                  // and this array of custom arguments to create our entry
+                "message": title
+			}]
+        ); 
+     }
+}
+module.exports = echo;
